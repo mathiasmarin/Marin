a36584a44048d8302e47a0d22647058a7a5f5380
Mathias Marin <mathiasmarin86@gmail.com>
2017-11-20 13:30:01 +0100

    Added boostrap datepicker. Added better styling for breadcrumb.

---
+++ Marin/wwwroot/js/CreateMonthlyBudget.js
@@ -0,0 +1,38 @@
+﻿(function () {
+    var app = new Vue({
+        created: function () {
+            Vue.nextTick(function() {
+                $('#datePicker').datepicker({
+                    autoclose: true,
+                    minViewMode: 1,
+                    format: 'yyyy-mm',
+                    language: 'sv',
+                    endDate: new Date().toJSON().slice(0, 10)
+                }).show();
+            });
+
+        },
+        data: function () {
+            return {
+                user: {}
+            }
+        },
+        methods: {
+            getUserInfo: function () {
+                const self = this;
+                $.ajax({
+                    url: apiUrl + "User",
+                    type: "GET",
+                    success: function (response) {
+                        if (response) {
+                            self.user = response;
+                        }
+                    },
+                    error: function (error) {
+
+                    }
+                });
+            }
+        }
+    }).$mount("#createMonthlyBudgetApp");
+})();
